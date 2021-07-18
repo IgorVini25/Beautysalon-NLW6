33902a883b32ce3578c1bbe05ba8b69f29a37030
IgorVini25 <igvinps2006@gmail.com>
2021-07-18 18:03:16 -0300

    Abas 'Sobre nós' e 'Serviços' criadas, menu funcional criado, scroll suave e outros detalhes

---
+++ main.js
@@ -0,0 +1,30 @@
+// Abre e fecha o menu quando icone for clicado
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// quando clicar em um item do menu, esconder um menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// Mudar o Header quando der scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
