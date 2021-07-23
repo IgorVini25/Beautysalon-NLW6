057d60cecdcec077e1a003d3eb1c6e4dabb15f7f
IgorVini25 <igvinps2006@gmail.com>
2021-07-23 12:58:45 -0300

    Footer criado, botão de voltar para o topo, animações extras e novo pacote de icones

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for (const link of links) {
 }
 
 // Mudar o Header quando der scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 // Avisar Número Fictício
 const aviso = document.querySelector('#contact a.button')
@@ -59,7 +59,31 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #sevices header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+// Botão voltar pro topo
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+  if (window.scrollY >= 3807) {
+    backToTopButton.classList.add('footer')
+  } else {
+    backToTopButton.classList.remove('footer')
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
